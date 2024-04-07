df49d2a8ed3af4c873bee5990964736c5314a170
jlitwinczuk <166211283+jlitwinczuk@users.noreply.github.com>
2024-04-07 12:41:42 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import geometry.Point;
+import geometry.Circle;
+import geometry.ColoredCircle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point center = new Point(0, 0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "yellow");
+
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + coloredCircle.getRadius());
+        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
+        System.out.println("Pole powierzchni koła: " + coloredCircle.getArea());
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
+    }
+}
